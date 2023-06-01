4e7800db994ed2075122ba68f5f9af0068127b54
Thien Nguyen <nqthien041292@gmail.com>
2023-06-01 11:26:37 +0700

    Implement oAuth0

--- client/src/config.ts
+++ client/src/config.ts
@@ -4,7 +4,7 @@ export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-dlx87j2b7mpd5lph.us.auth0.com',            // Auth0 domain
+  clientId: 'QI1ox9KghdOwtUV0RCM0VM5acBjpy1iq',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
