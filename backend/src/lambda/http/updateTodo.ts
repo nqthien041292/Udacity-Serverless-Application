a05da6de9579daf91b8307f8b7cb18ef1597c84e
Thien Nguyen <nqthien041292@gmail.com>
2023-06-01 10:45:50 +0700

    Implement updateTodo

--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -3,11 +3,32 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
 
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+import { updateTodo } from '../../businessLogic/todos'
+import { createLogger } from '../../utils/logger'
+
+const logger = createLogger('updateTodohandler')
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
-  const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
 
-  // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-  return undefined
+  // DONE: Update a TODO item with the provided id using values in the "updatedTodo" object
+  logger.info('Processing event: ', event)
+  const updateTodoRequest: UpdateTodoRequest = JSON.parse(event.body)
+
+  // DONE: Implement creating a new TODO item
+  const authorization = event.headers.Authorization
+  const split = authorization.split(' ')
+  const jwtToken = split[1]
+
+  await updateTodo(todoId, updateTodoRequest, jwtToken)
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify(updateTodoRequest)
+  }
+
 }
