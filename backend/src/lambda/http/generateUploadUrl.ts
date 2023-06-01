b79e9e5257fbf0edaf7bba4e195b566113d2b762
Thien Nguyen <nqthien041292@gmail.com>
2023-06-01 11:07:32 +0700

    1. Upload url is returned in the response 2. Update attachmentUrl in the todos table

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -1,10 +1,57 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { generateAndAddUploadUrl, todoExists } from '../../businessLogic/todos'
+import { createLogger } from '../../utils/logger'
+
+const logger = createLogger('generateUploadUrlhandler')
+
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
 
   // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
+  const authorization = event.headers.Authorization
+  const split = authorization.split(' ')
+  const jwtToken = split[1]
+  logger.info('Processing event: ', event)
+  const isValidTodoId = await todoExists(todoId, jwtToken)
+
+  if (!isValidTodoId) {
+    return {
+      statusCode: 404,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify({
+        error: 'Todo not found'
+      })
+    }
+  }
+
+  try {
+    const uploadUrl = await generateAndAddUploadUrl(todoId, jwtToken)
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify({
+        uploadUrl: uploadUrl
+      })
+    }
+  } catch (err) {
+    logger.error('Failed to generate url', err);
+    return {
+      statusCode: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: 'Failed to generate url',
+    }
+  }
 }
