5b9f3692b1c0096f76a0cdb2661c08e4ed2806ab
Thien Nguyen <nqthien041292@gmail.com>
2023-06-01 10:49:52 +0700

    Implement deleteTodo

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -1,10 +1,54 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { createLogger } from '../../utils/logger'
+import { deleteTodo, todoExists } from '../../businessLogic/todos'
+
+const logger = createLogger('deleteTodohandler')
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
 
-  // TODO: Remove a TODO item by id
-  return undefined
+  // DONE: Remove a TODO item by id
+  const authorization = event.headers.Authorization
+  const split = authorization.split(' ')
+  const jwtToken = split[1]
+  logger.info('Processing event: ', event)
+  const isValidTodoId = await todoExists(todoId, jwtToken)
+
+  if (!isValidTodoId) {
+    return {
+      statusCode: 404,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify({
+        error: 'Todo not found'
+      })
+    }
+  }
+
+  try {
+    await deleteTodo(todoId, jwtToken)
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: 'Deleted'
+    }
+  } catch (err) {
+    logger.error('Failed to delete', err);
+    return {
+      statusCode: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: 'Failed to delete',
+    }
+  }
 }
